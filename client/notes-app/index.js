b39a33df2e90e80be2cf2c65d5a473dc8ad0d866
otopba <otopba@inbox.ru>
2017-05-29 01:17:35 +0300

    add redux

--- client/notes-app/index.js
+++ client/notes-app/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 ReactDOM.render(
-<h1>Hello, world!1wwwwwww</h1>,
+<h1>Hello, world!1ww33333wwwww</h1>,
     document.getElementById('root')
 );
